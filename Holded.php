1f8750861679901088c5757b4fae6ba6b0159620
José Pedro Andrés García <macklus@debianitas.net>
2018-01-02 09:52:07 +0100

    * Fix error and get more info
    - Fix an error when we miss $extra array
    - Get more data on response

--- Holded.php
+++ Holded.php
@@ -87,7 +87,7 @@ class Holded extends BaseObject
 
     public function getinvoices($extra = null)
     {
-        return $this->get('/get/doc/invoice', null);
+        return $this->get('/get/doc/invoice', $extra);
     }
 
     public function findinvoice($extra)
@@ -360,6 +360,8 @@ class Holded extends BaseObject
 
         if ($db->save() === true) {
             $data['holded_id'] = $db->id;
+            if (isSet($db->invoicenum)) $data['invoicenum'] = $db->invoicenum;
+            if (isSet($db->contactid)) $data['contactid'] = $db->contactid;
         }
 
         return $data;
